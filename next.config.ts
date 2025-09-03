2fa59cfa89d67067dbfac6393699dc37d3cfcea8
saroro <211804464+52galuser@users.noreply.github.com>
2025-09-03 21:58:02 +0900

    feat: Add Swagger

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,16 @@
 import type { NextConfig } from 'next';
 
+const backEndPort = process.env.BACKEND_PORT || 4000;
 const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:4000/:path*',
+        source: '/api/v1/:path*',
+        destination: `http://localhost:${backEndPort}/api/v1/:path*`,
+      },
+      {
+        source: '/docs/:path*', // /docs 접속하면
+        destination: `http://localhost:${backEndPort}/docs/:path*`,
       },
     ];
   },
